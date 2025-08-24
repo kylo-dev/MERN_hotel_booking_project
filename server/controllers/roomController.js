a1d3c073ab891b70c20a7a751ebe823b4bd9d14b
Kim HyunGyeom <103489352+kylo-dev@users.noreply.github.com>
2025-08-24 23:49:00 +0900

    feature: booking API 구현

--- server/controllers/roomController.js
+++ server/controllers/roomController.js
@@ -1,4 +1,3 @@
-import { populate } from "dotenv";
 import Hotel from "../models/Hotel.js";
 import Room from "../models/Room.js";
 import { v2 as cloudinary } from "cloudinary";
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import userRouter from "./routes/userRoutes.js";
 import hotelRouter from "./routes/hotelRoutes.js";
 import roomRouter from "./routes/roomRoutes.js";
 import connectCloudinary from "./configs/cloudinary.js";
+import bookingRouter from "./routes/bookingRoutes.js";
 
 // DB Connect
 connectDB();
@@ -29,6 +30,7 @@ app.get("/", (req, res) => res.send("API is fine"));
 app.use("/api/users", userRouter);
 app.use("/api/hotels", hotelRouter);
 app.use("/api/rooms", roomRouter);
+app.use("/api/bookings", bookingRouter);
 
 const PORT = process.env.PORT || 3000;
 
