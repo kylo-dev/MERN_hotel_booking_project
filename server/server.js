f69396e96159c1b96e7fca58fb02732818f8d76f
Kim HyunGyeom <103489352+kylo-dev@users.noreply.github.com>
2025-08-24 18:50:29 +0900

    feature: User 조회 및 최근 검색 호텔 업데이트 API 추가

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
+import userRouter from "./routes/userRoutes.js";
 
 // DB Connect
 connectDB();
@@ -19,6 +20,9 @@ app.use(clerkMiddleware());
 app.use("/api/clerk", clerkWebhooks);
 
 app.get("/", (req, res) => res.send("API is fine"));
+
+// Router 연결
+app.use("/api/user", userRouter);
 
 const PORT = process.env.PORT || 3000;
 
